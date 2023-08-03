15d03295f0eb8bb6d24ea00c5d1349e1714de88c
Mimansa Kumari <mimansakumari1@gmail.com>
2023-08-03 08:44:50 +0530

    Add files via upload

---
+++ Practice/demo/src/main/java/com/jap/demo/SprintBootS4Pc1Application.java
@@ -0,0 +1,15 @@
+package com.jap.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootS4Pc1Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SprintBootS4Pc1Application.class, args);
+		System.out.println("Good Day To You!");
+	}
+
+}
---
+++ Practice/demo/src/test/java/com/jap/demo/SprintBootS4Pc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jap.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootS4Pc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
